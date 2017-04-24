37dba13fc0b11da64a5d8a9dab3b54d62b24f936
German Escobar <german.escobarc@gmail.com>
2017-04-24 17:19:28 -0500

    Implement list and show courses

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: subjects
+#
+#  id         :integer          not null, primary key
+#  course_id  :integer
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Subject < ApplicationRecord
   belongs_to :course
 end
